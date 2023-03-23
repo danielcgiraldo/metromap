faaa4a6f85e52cad2ee10833ffe9a598dced8979
Santiago Rivera Mejia <sriverame@unal.edu.co>
2023-03-23 14:52:43 -0500

    Updated Views.py
    Emojis work now, no implementation of REST tho

--- core/myapp/views.py
+++ core/myapp/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from SNT import gettweets
 from django.http import JsonResponse
-import json
 # Create your views here.
 
 def get_tweets_endpoint(request):
     tweets = gettweets()
-    return JsonResponse(tweets, safe=False)
+    return JsonResponse(tweets, safe=False, json_dumps_params={'ensure_ascii': False})
+
