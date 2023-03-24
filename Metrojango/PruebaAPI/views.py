d6fa72cf6cecf7a4827e3bac5b042840b24bd7c8
JPortoL <jporto@unal.edu.co>
2023-03-23 19:43:23 -0500

    ApiDePrueba

---
+++ Metrojango/PruebaAPI/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+
+# Create your views here.
+
+def tweets(request):
+    url = "https://demo-api-metromap.vercel.app/v1/tweets"
+    response = requests.get(url)
+    api = response.json()
+    tweet_info = []
+    for tweet in api['data']:
+        tweet_dict = {}
+        tweet_dict['url'] = tweet['url']
+        tweet_dict['content'] = tweet['content']
+        tweet_info.append(tweet_dict)
+    context = {"tweet_info": tweet_info, "api_url": url}
+    return render(request, 'tweets.html', context)
