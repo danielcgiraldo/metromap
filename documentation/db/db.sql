97f1bf9deaccbbcbcc048f840fa175f70d3c38df
Daniel Castillo Giraldo <dc.giraldocg@hotmail.com>
2023-03-29 23:44:00 -0500

    3 tables db

--- documentation/db/db.sql
+++ documentation/db/db.sql
@@ -5,3 +5,18 @@ CREATE TABLE LINE(
     color CHAR(6) NOT NULL COMMENT 'HEX code of the color',
     type ENUM('M', 'C', 'TB', 'B') NOT NULL COMMENT 'type must be: M, C, TB, B: metro, cable, trolley and electric bus, bus.'
 );
+
+DROP TABLE IF EXISTS STATION;
+CREATE TABLE STATION(
+    line CHAR(1) NOT NULL COMMENT 'FK of line(id)',
+    id VARCHAR(40) NOT NULL,
+    sites_of_interest JSON NULL,
+    services JSON NULL,
+    status ENUM('O', 'P', 'M', 'U') NOT NULL COMMENT 'status must be: O, P, M, U: operational, partial outage (closed station with transit), major outage, under maintenance.'
+);
+
+DROP TABLE IF EXISTS ALIAS;
+CREATE TABLE ALIAS(
+    station VARCHAR(40) NOT NULL COMMENT 'FK of station(id)',
+    alternate VARCHAR(100) NOT NULL COMMENT 'Name of the station'
+);
