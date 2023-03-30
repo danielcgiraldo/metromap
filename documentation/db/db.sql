bb50554f6ab8b66e6d4cc32ae1d2eb1d4065b5bb
JPortoL <jporto@unal.edu.co>
2023-03-30 18:49:43 -0500

    db user y report
    Adding the user and report entities to the db

--- documentation/db/db.sql
+++ documentation/db/db.sql
@@ -20,3 +20,24 @@ CREATE TABLE ALIAS(
     station VARCHAR(40) NOT NULL COMMENT 'FK of station(id)',
     alternate VARCHAR(100) NOT NULL COMMENT 'Name of the station'
 );
+
+DROP TABLE IF EXISTS REPORT;
+CREATE TABLE REPORT(
+    id VARCHAR(40) NOT NULL,
+    title VARCHAR(100) NOT NULL COMMENT 'title of the report',
+    type ENUM('V', 'S', 'L', 'C', 'W') NOT NULL COMMENT 'type must be: V, S, L, C, W: vehicles, stations, lines, contact means, webpage.',
+    description VARCHAR(250) NULL COMMENT 'description of the report',
+    date DATETIME NOT NULL COMMENT 'date and time the report was uploaded'
+);
+
+DROP TABLE IF EXISTS USER;
+CREATE TABLE USER(
+    email VARCHAR(100) NOT NULL COMMENT 'email of the user',
+    pwd VARCHAR(100) NOT NULL COMMENT 'password of the user',
+    name VARCHAR(50) NOT NULL COMMENT 'name of the user',
+    status ENUM('1', '0') NOT NULL COMMENT 'status must be: 1, 0: active, banned.',
+    credits INT(4) NOT NULL COMMENT 'number of credits of the user, credits is used to limit the amount of request to the API. Resets every month to 40.',
+    secret_key VARCHAR(100) NOT NULL COMMENT 'not public key of the user, is a unique alternative identifiers',
+    public_key VARCHAR(100) NOT NULL COMMENT 'public key of the user, is a unique alternative identifiers',
+    allowed_domains JSON NOT NULL COMMENT 'domains to which the user has access'
+);
