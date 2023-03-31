ab6ce7ea55e0acdc3563861de6c410fc0fae001d
JPortoL <jporto@unal.edu.co>
2023-03-30 23:39:40 -0500

    stations

---
+++ core/api/modules/scrapping.py
@@ -0,0 +1,8 @@
+def scrapping(tweet, id):
+    """La función retorna un lista de listas donde cada sublista contiene la url,
+    el contenido y el gif de cada tweet individual desde el tweet de entrada hasta
+    el último tweet en la base de datos.
+
+    Esta recibe el último tweet registrado con el id
+    """
+    pass
