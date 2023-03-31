4f7859940ca0564e1966239f651efcd9a357b218
Santiago Rivera Mejia <sriverame@unal.edu.co>
2023-03-31 13:20:53 -0500

    Fixed Header Secret key

--- core/api/views.py
+++ core/api/views.py
@@ -12,63 +12,63 @@ def get_tweets_endpoint(request):
 
 def get_status(request):
     # checks if secret key credentials are in the header
-    sk = request.META.get("secret_key")
+    sk = request.META.get("HTTP_SECRET_KEY")
     if (sk == None):
        return JsonResponse({'status':'error', 'error':'invalid_client_credentials', 'description':'secret_key not received',}, status=403)
     return JsonResponse({'status':'error', 'error':'under_maintenance','message':'Map in under maintenace'})
 
 def get_status_line(request, line):
     # checks if secret key credentials are in the header
-    sk = request.META.get("secret_key")
+    sk = request.META.get("HTTP_SECRET_KEY")
     if (sk == None):
        return JsonResponse({'status':'error', 'error':'invalid_client_credentials', 'description':'secret_key not received',}, status=403)
     return JsonResponse({'status':'error', 'error':'under_maintenance','message':'Map in under maintenace'})
 
 def get_status_station(request, line, station):
     # checks if secret key credentials are in the header
-    sk = request.META.get("secret_key")
+    sk = request.META.get("HTTP_SECRET_KEY")
     if (sk == None):
        return JsonResponse({'status':'error', 'error':'invalid_client_credentials', 'description':'secret_key not received',}, status=403)
     return JsonResponse({'status':'error', 'error':'under_maintenance','message':'Map in under maintenace'})
 
 def get_data(request):
     # checks if secret key credentials are in the header
-    sk = request.META.get("secret_key")
+    sk = request.META.get("HTTP_SECRET_KEY")
     if (sk == None):
        return JsonResponse({'status':'error', 'error':'invalid_client_credentials', 'description':'secret_key not received',}, status=403)
     return JsonResponse({'status':'error', 'error':'under_maintenance','message':'Map in under maintenace'})
 
 def get_data_line(request, line):
     # checks if secret key credentials are in the header
-    sk = request.META.get("secret_key")
+    sk = request.META.get("HTTP_SECRET_KEY")
     if (sk == None):
        return JsonResponse({'status':'error', 'error':'invalid_client_credentials', 'description':'secret_key not received',}, status=403)
     return JsonResponse({'status':'error', 'error':'under_maintenance','message':'Map in under maintenace'})
 
 def get_data_station(request, line, station):
     # checks if secret key credentials are in the header
-    sk = request.META.get("secret_key")
+    sk = request.META.get("HTTP_SECRET_KEY")
     if (sk == None):
        return JsonResponse({'status':'error', 'error':'invalid_client_credentials', 'description':'secret_key not received',}, status=403)
     return JsonResponse({'status':'error', 'error':'under_maintenance','message':'Map in under maintenace'})
 
 def get_incident(request):
     # checks if secret key credentials are in the header
-    sk = request.META.get("secret_key")
+    sk = request.META.get("HTTP_SECRET_KEY")
     if (sk == None):
        return JsonResponse({'status':'error', 'error':'invalid_client_credentials', 'description':'secret_key not received',}, status=403)
     return JsonResponse({'status':'error', 'error':'under_maintenance','message':'Map in under maintenace'})
  
 def get_incident_line(request, line):
     # checks if secret key credentials are in the header
-    sk = request.META.get("secret_key")
+    sk = request.META.get("HTTP_SECRET_KEY")
     if (sk == None):
        return JsonResponse({'status':'error', 'error':'invalid_client_credentials', 'description':'secret_key not received',}, status=403)
     return JsonResponse({'status':'error', 'error':'under_maintenance','message':'Map in under maintenace'})
 
 def get_incident_station(request, line, station):
     # checks if secret key credentials are in the header
-    sk = request.META.get("secret_key")
+    sk = request.META.get("HTTP_SECRET_KEY")
     if (sk == None):
        return JsonResponse({'status':'error', 'error':'invalid_client_credentials', 'description':'secret_key not received',}, status=403)
     return JsonResponse({'status':'error', 'error':'under_maintenance','message':'Map in under maintenace'})
