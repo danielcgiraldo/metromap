566c53cb6abca38db88af064ecf8468fd4c07b07
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-04-01 00:01:05 -0500

    home and status page

--- core/core/settings.py
+++ core/core/settings.py
@@ -51,8 +51,10 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
+
+X_FRAME_OPTIONS = 'ALLOWALL'
 
 CORS_ORIGIN_ALLOW_ALL = True
 
