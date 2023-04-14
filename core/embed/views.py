d4195d6cafd852bbd496e3b61cbc9f3cf34c9387
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-04-13 22:14:10 -0500

    map visual

--- core/embed/views.py
+++ core/embed/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.template import loader
 
 # Create your views here.
 
 # Creamos la vista 'tweets'
 def map(request):
     # Check if public_key is in url
-    key = request.GET.get('public-key', '')
-    if(key == ""):
-        return JsonResponse({"status":"error", "error": "invalid_client_credentials", "description": "public-key not received"}, status=403)
-    return JsonResponse({"status":"error", "error": "under_maintenance", "message": "Map is under maintenance"})
-    
+    template = loader.get_template('map.html')
+    return HttpResponse(template.render())
