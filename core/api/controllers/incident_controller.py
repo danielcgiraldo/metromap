0bd20ff1c9695c7bf786ff7394685f84e3c50aaa
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-04-20 18:32:42 -0500

    auth line, station exists

--- core/api/controllers/incident_controller.py
+++ core/api/controllers/incident_controller.py
@@ -1,10 +1,17 @@
+from api.models import Line, Station, Incident, AffectedStation, Notification
 class Incident:
     def __init__(self, line, station):
         self.line = line
         self.station = station
 
-    def get_data():
+    def get_data(self):
         """
         Return data according to request
         """
+        # TODO: dt GET.VALUE
+
+        
+        incidents = Incident.objects.filter(status=1)
+
+            
         return {"A": {"san_antonio": {"status": "O", "tweet_id": 123}}}
