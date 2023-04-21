fb92a301b67ed4d2b5e30cfb3a30a501a47caccf
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-04-20 19:18:56 -0500

    incident endpoint

--- core/api/controllers/incident_controller.py
+++ core/api/controllers/incident_controller.py
@@ -1,4 +1,4 @@
-from api.models import Line, Station, Incident, AffectedStation, Notification
+from api.models import Incident as Incidents, AffectedStation, Notification
 class Incident:
     def __init__(self, line, station):
         self.line = line
@@ -10,8 +10,27 @@ class Incident:
         """
         # TODO: dt GET.VALUE
 
-        
-        incidents = Incident.objects.filter(status=1)
-
-            
-        return {"A": {"san_antonio": {"status": "O", "tweet_id": 123}}}
+        data = {}
+        # FIXME: Current classname to Incidents 
+        incidents = Incidents.objects.filter(status=1)
+        for incident in incidents:
+            tweet_id = (Notification.objects.filter(incident=incident).first()).tweet_id
+            if self.station:
+                affected_stations = AffectedStation.objects.filter(incident=incident, affected_station=self.station)
+            else:
+                affected_stations = AffectedStation.objects.filter(incident=incident)
+            for affected_station in affected_stations:
+                station = affected_station.affected_station
+                if self.station:
+                    if self.station == station.station:
+                        data[station.line][station.station] = {"status": station.status, "tweet_id": tweet_id}
+                elif self.line:
+                    if self.line == station.line:
+                        if station.line.id not in data:
+                            data[station.line.id] = {}
+                        data[station.line.id][station.station] = {"status": station.status, "tweet_id": tweet_id}
+                else:
+                    if station.line.id not in data:
+                        data[station.line.id] = {}
+                    data[station.line.id][station.station] = {"status": station.status, "tweet_id": tweet_id}
+        return data
