1130a7ff1466959c857f1c7a2f2bd44860393ffc
Daniel Castillo <dc.giraldocg@hotmail.com>
2023-04-28 11:34:50 -0500

    api requests

--- core/embed/urls.py
+++ core/embed/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import map
+from .views import map, check_incoming, get_data, get_incidents, get_update
 
-# Añadimos la vista tweets a las urls
+# Map and API endpoints map uses.
+# Endpoints work using api.metromap.online requests.
 urlpatterns = [
     path("v1/map/", map),
+    path("/api/v1/incident/<str:line>/<str:station>/", lambda request,
+         line, station: check_incoming(request, get_incidents, line, station)),
+    path("/api/v1/data/<str:line>/<str:station>/", lambda request, line,
+         station: check_incoming(request, get_data, line, station)),
+    path("/api/v1/update/", lambda request: check_incoming(request, get_update))
 ]
--- core/embed/views.py
+++ core/embed/views.py
@@ -12,3 +12,39 @@ def map(request):
     # Render map in /templates/map.html
     template = loader.get_template('map.html')
     return HttpResponse(template.render())
+
+def check_incoming(request, fn, line = None, station = None):
+    """
+    Check if the request is made from map, and not someone else.
+
+    Parameters:
+        request (django.http.request.HttpRequest): The request object.
+        fn (function): The function to be executed if the request is valid.
+        line (str): The line of the station.
+        station (str): The station.
+
+    Returns:
+        If the request is valid, the function returns the result of fn.
+    """
+
+    # TODO: Check public key in request
+    
+    # Get referer
+    referer = request.META.get('HTTP_REFERER', '')
+
+    # Check if referer is valid
+    if referer and not referer.startswith(request.scheme + '://' + request.get_host()):
+        return JsonResponse({"status":"error", "error": "invalid_referer", "description": "referer is not metromap.online"}, status=403)
+    if line:
+        return fn(line, station)
+    else:
+        return fn()
+
+def get_data(line, station):
+    pass
+
+def get_update():
+    pass
+
+def get_incidents(line, station):
+    pass
