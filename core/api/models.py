048182b3005aec22d53f99841f629b7dd2068cac
JPortoL <jporto@unal.edu.co>
2023-05-12 10:35:42 -0500

    update method

--- core/api/models.py
+++ core/api/models.py
@@ -104,7 +104,6 @@ class Report(models.Model):
 
 class User(models.Model):
     email = models.EmailField(primary_key=True)
-    name = models.CharField(max_length=50)
     status = models.BooleanField()
     credits = models.IntegerField()
     secret_key = models.CharField(max_length=20, unique=True)
--- core/api/views.py
+++ core/api/views.py
@@ -29,15 +29,17 @@ def get_incident(line, station, GET):
     return JsonResponse({'status': 'ok', 'data': incident.get_data()})
 
 
-def user(request, type, email):
+def user(request, type, email, GET):
     secret = request.get("secret-key", None)
     if secret != "nyyL6d36KnC%ju38Vr5^":
         return JsonResponse({'status': 'error', 'error': 'invalid_client_credentials', 'description': 'secret-key not received', }, status=403)ßß
-    user = UserCredentials(email)
+    user = UserCredentials(email, GET)
     if type == "set":
         return user.set()
     if type == "get":
         return user.get()
+    if type == "update":
+        return user.update()
     else:
         return JsonResponse({'status': 'error', 'error': 'not_found',
                              'description': 'not valid method'}, status=404)
