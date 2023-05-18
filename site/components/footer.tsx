d2a5e19a5052d96a32ea4fda16b0de4d338d0eb1
JPortoL <jporto@unal.edu.co>
2023-05-17 23:46:53 -0500

    lil changes contris

--- site/components/footer.tsx
+++ site/components/footer.tsx
@@ -19,10 +19,10 @@ const Contributors = () => {
 
   return (
     <div>
-      
+      <h2 style={{ color: '#333', textAlign: 'center' }}>Contribuidores</h2>
       <ul style={{ marginTop: '10px', listStyle: 'none', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         {contributors.map((contributor) => (
-          <li key={contributor.id} style={{ margin: '0 50px', textAlign: 'center' }}>
+          <li key={contributor.id} style={{ margin: '0 15px', textAlign: 'center' }}>
             <a href={contributor.html_url} target="_blank" rel="noopener noreferrer">
               <img src={contributor.avatar_url} alt={contributor.login} style={{ width: '100px', borderRadius: '50%' }} />
               <p style={{ marginTop: '10px', color: '#123' }}>{contributor.name || contributor.login}</p>
